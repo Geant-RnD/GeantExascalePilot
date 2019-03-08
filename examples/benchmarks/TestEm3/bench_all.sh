cb5d4600178414b003b6a798badf23de7887d64c
Philippe Canal <pcanal@fnal.gov>
2019-03-08 16:17:17 -0600

    Remove unused examples

--- examples/benchmarks/TestEm3/bench_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bash bench_GV_all.sh "$1"
-bash TestEm3_G4.run "$1"
--- examples/physics/FullCMS/Geant4/src/MyTrackInformation.cc
+++
@@ -1,6 +0,0 @@
-
-#include "MyTrackInformation.hh"
-
-MyTrackInformation::MyTrackInformation(G4int id) : fPrimaryTrackID(id) {}
-
-MyTrackInformation::~MyTrackInformation() {}
