a715f8c741e08279b99c1e33c9510b2aec5f0529
Soon yung Jun <syjun@lq.fnal.gov>
2020-01-02 16:30:20 -0600

    add constants for energy loss processes

--- source/Geant/proxy/ProxyConstants.hpp
+++ source/Geant/proxy/ProxyConstants.hpp
@@ -14,17 +14,28 @@
 
 #pragma once
 
-namespace geantx {
+#include "Geant/core/SystemOfUnits.hpp"
 
-namespace brem {
-  
+namespace geantx {
+namespace data {
+
+constexpr int nParticleForCuts =  4; 
+
+constexpr double dRoverRange = 0.2; // 20%
+constexpr double finalRange  = 10*geantx::units::mm ; //CLHEP::mm, with pre-build Geant4 tables 
+
+// ProxyPhysicsVector
+constexpr int maxPhysicsVector =  3;
+constexpr int maxPVDataVector  = 78;
+
 // Proxy2DVector (SeltzerBerger data) - valid up to maximumZ = 92
 constexpr int maximumZ                = 92;
 constexpr unsigned int numberOfXNodes = 32;
 constexpr unsigned int numberOfYNodes = 57;
 
+// will be removed later
 constexpr double xgi[8] = {0.0199, 0.1017, 0.2372, 0.4083, 0.5917, 0.7628, 0.8983, 0.9801};
 constexpr double wgi[8] = {0.0506, 0.1112, 0.1569, 0.1813, 0.1813, 0.1569, 0.1112, 0.0506};
-} // namespace brem
-  
+
+} // namespace data
 } // namespace geantx
