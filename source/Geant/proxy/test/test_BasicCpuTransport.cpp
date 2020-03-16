128a62b55ed416398aa4023c2ec9eaa672eb07c5
Philippe Canal <pcanal@fnal.gov>
2020-03-16 14:07:40 -0500

    Move the change to fPhysicsState.fPstep to only update for the winning process

--- source/Geant/proxy/test/test_BasicCpuTransport.cpp
+++ source/Geant/proxy/test/test_BasicCpuTransport.cpp
@@ -459,6 +459,8 @@ OneStep(Track *track)
     ///          return
     Apply<void>::unroll_indices<AlongStepApply_t>(track, &doit_idx, &proposedPhysLength, &doit_apply);
 
+    track->fPhysicsState.fPstep = proposedPhysLength;
+
     NavigationInterface::FindNextBoundary(*track);
 
     /// Apply multiple scaterring if any.
