33f19a633c93d3ab02d7023d8002b0b125a0fb20
Soon yung Jun <syjun@lq.fnal.gov>
2020-05-07 13:27:03 -0500

    update for proxy physics test/validation

--- source/Geant/proxy/validation/test_process.cu
+++ source/Geant/proxy/validation/test_process.cu
@@ -1,94 +1,47 @@
-
-#include <iostream>
-#include <stdio.h>
-#include <vector>
-
-#include <cuda_runtime.h>
-
-#include "Geant/proxy/ProxyPhysicalConstants.hpp"
-
-#include "Geant/processes/Process.hpp"
-#include "Geant/proxy/ProxyEmProcess.hpp"
-#include "Geant/proxy/ProxyCompton.hpp"
-#include "Geant/proxy/ProxyEmModel.hpp"
-#include "Geant/proxy/ProxyKleinNishina.hpp"
-
-#include "Geant/proxy/ProxyPhysicsTableIndex.hpp"
-#include "Geant/proxy/ProxyPhysicsVector.cuh"
-#include "Geant/proxy/ProxyPhysicsTable.cuh"
-#include "Geant/proxy/ProxyDataManager.cuh"
+#include "Geant/proxy/ProxyRunManager.hpp"
+#include "Geant/proxy/ProxyHepEvtGenerator.hpp"
+#include "Geant/proxy/ProxyEventServer.hpp"
+#include "Geant/proxy/ProxyDeviceManager.hpp"
 
 #ifndef ADDED_SUPPORT_FOR_SEPARABLE_COMPILATION
-#include "Geant/processes/src/Process.cpp"
-#include "Geant/proxy/src/ProxyEmProcess.cpp"
-#include "Geant/proxy/src/ProxyCompton.cpp"
-#include "Geant/proxy/src/ProxyEmModel.cpp"
-#include "Geant/proxy/src/ProxyKleinNishina.cpp"
-
-#include "Geant/proxy/src/ProxyPhysicsVector.cu"
-#include "Geant/proxy/src/ProxyPhysicsTable.cu"
-#include "Geant/proxy/src/ProxyDataManager.cu"
-
+#include "Geant/proxy/src/ProxyRunManager.cpp"
+#include "Geant/proxy/src/ProxyHepEvtGenerator.cpp"
+#include "Geant/proxy/src/ProxyEventServer.cpp"
+#include "Geant/proxy/src/ProxyDeviceManager.cpp"
 #endif
 
 using namespace geantx;
 
-__host__ __device__ void validation_process(double lambda) 
-{
-  //TODO: add validation items with input TrackState and check GPIL
-  printf("Lambda = %g\n", lambda);
-}
-
-void cpu_process()
-{
-  // a process on the host
-  ProxyEmProcess<ProxyCompton> *proc = new ProxyEmProcess<ProxyCompton>();
-
-  //TODO: add validation items with input TrackState and check GPIL
-  validation_process(proc->GetLambda(1,10));
-
-  delete proc;
-}
-
-__global__ void gpu_process(ProxyDataManager *dm_d)
-{
-  unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
-
-  ProxyEmProcess<ProxyCompton> proc(tid);
-  ProxyKleinNishina model(tid);
-
-  proc.SetModel(&model);
-  proc.SetDataManager(dm_d);
-
-  //TODO: add validation items with input TrackState and check GPIL
-  validation_process(proc.GetLambda(1,10));
-}
-
 int main(int argc, char* argv[]) {
 
-  // set the default number of threads and thread blocks
-  int theNBlocks  =  1;
-  int theNThreads =  1;
+  //configuration - arguments
 
-  if(argc >= 2) theNBlocks = atoi(argv[1]);
-  if(argc >= 3) theNThreads = atoi(argv[2]);
+  const int number_of_events_to_process = 50;
+  const char* event_filename = "hepevt.data";
 
-  ProxyDataManager * dm = ProxyDataManager::Instance();
+  // run manager - register generator, detector, physics list and user actions
 
-  // relocate ProxyDataManager to the device
-  // TODO: put this step into an initialization of a coprocessor manager
-  ProxyDataManager *dm_d;
-  cudaMalloc((void **)&dm_d, dm->SizeOfObject());
-  dm->RelocatePhysicsData(dm_d);
+  ProxyRunManager<ProxyHepEvtGenerator> *runMgr 
+    = ProxyRunManager<ProxyHepEvtGenerator>::Instance();
 
-  // CPU kernel
-  cpu_process();
+  ProxyHepEvtGenerator* agen = 
+    new ProxyHepEvtGenerator(number_of_events_to_process,
+                             event_filename);
 
-  // GPU kernel
-  gpu_process<<<theNBlocks,theNThreads>>>(dm_d);
-  cudaDeviceSynchronize();
+  runMgr->RegisterEventGenerator(agen);
+  
+  // test
+  runMgr->GetEventManager()->GenerateEvents(50);
 
-  cudaFree(dm_d);
+  runMgr->GetDeviceManager()->SetPerformanceFlag(true);
+
+  runMgr->GetDeviceManager()->UploadTrackData();
+
+  runMgr->GetDeviceManager()->DoStep();
+
+  runMgr->GetDeviceManager()->DownloadTrackData();
+
+  runMgr->GetDeviceManager()->DeallocateTrackData();
 
   return 0;
 }
